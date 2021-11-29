5b384500061c829a0c6302c74881c2d4a9878a41
CreePearl616 <83845072+CreePearl616@users.noreply.github.com>
2021-11-29 01:28:31 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -136,7 +136,7 @@ clear
 figlet -c -f slant -t 'Tiringa-BOT' | lolcat 
 
 echo -e " ${NOCOLOR}"
-echo -e " ${PURPLE} Instalando módulos npm. Sol sua Linda"
+echo -e " ${PURPLE} Instalando módulos npm Sol sua Linda."
 echo -e " ${GREEN}"
 
 npm i node-tesseract-ocr --no-bin-links
@@ -190,4 +190,4 @@ echo ""
 
 echo -e " ${YELLOW} Todas as dependências foram instaladas."
 echo -e " ${YELLOW} Atualização: mudanças mínimas"
-echo -e " ${YELLOW} Use o comando: ${GREEN}npm start ${YELLOW}para iniciar o bot."
+echo -e " ${YELLOW} Use o comando: ${GREEN}npm start ${YELLOW}para iniciar o bot. Sol sua Linda."
