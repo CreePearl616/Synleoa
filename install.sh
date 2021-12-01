7bd897998ff573f9be64e531e0a51d3b07153225
CreePearl616 <83845072+CreePearl616@users.noreply.github.com>
2021-12-01 01:00:40 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,26 +17,163 @@ LIGHTCYAN='\033[1;36m'
 WHITE='\033[1;37m'
 
  echo -e " ${YELLOW}Iniciando..."
- read inp
-if [ "$inp" = "y" ]; then
-    pkg update
-    pkg upgrade
-    apt install ruby -y
-    gem install lolcat
-    install figlet -y
-    figlet -c -f slant -t 'Tiringa-BOT' | lolcat 
-    pkg install nodejs -y
-    pkg install libwebp -y
-    pkg install ffmpeg -y
-    pkg install wget -y
-    pkg install tesseract -y
-wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
+sleep 1
+clear
+
+if [ -e "$HOME/../usr/bin/ruby" ]; then
+	echo -e " ${GREEN}ruby detectado prosseguindo..."
+	sleep 1
+	if [ -e "/data/data/com.termux/files/usr/bin/lolcat" ]; then
+		echo -e " ${GREEN}lolcat detectado prosseguindo..."
+		sleep 1
+	else
+		echo -e " ${RED} lolcat não detectado, instalando..."
+		gem install lolcat
+	fi
+else
+	echo -e " ${RED}Você não instalou o ruby, instalando..."
+	sleep 1 
+	
+	apt install ruby -y
+	if [ -e "/data/data/com.termux/files/usr/bin/lolcat" ]; then
+		echo -e " ${GREEN}lolcat detectado prosseguindo..."
+		sleep 1
+	else
+		echo -e " ${RED}lolcat não detectado, instalando..."
+		gem install lolcat
+	fi
+fi 
 sleep 2
-echo -e " ${YELLOW} Todas que davam, foram instaladas."
+clear
+
+apt-get install figlet -y
+clear
+
+figlet -c -f slant -t 'Tiringa-BOT' | lolcat 
+
+echo -e " ${CYAN}A instalar todas as dependências..." | lolcat -a -d 50 
+
+echo -e " ${NOCOLOR}"
+echo -e " ${PURPLE} Usando o comando: pkg update"
+echo -e " ${GREEN}"
+sleep 1
+pkg update -y
+sleep 3
+clear
+figlet -c -f slant -t 'Tiringa-BOT' | lolcat 
+
+echo -e " ${NOCOLOR}"
+echo -e " ${PURPLE} Usando o comando: pkg upgrade"
+echo -e " ${GREEN}"
+sleep 1
+pkg upgrade -y
+sleep 3
+clear
+figlet -c -f slant -t 'Tiringa-BOT' | lolcat 
+
+if [ -e "$HOME/../usr/bin/nodejs" ]; then
+	echo -e " ${GREEN}nodejs detectado prosseguindo..."
+	sleep 1
+	else
+		echo -e " ${RED} nodejs não detectado, instalando..."
+echo -e " ${NOCOLOR}"
+echo -e " ${PURPLE} Usando o comando: pkg install nodejs"
+echo -e " ${GREEN}"
+sleep 1
+pkg install nodejs -y
+fi
+sleep 3
+clear
+figlet -c -f slant -t 'Tiringa-BOT' | lolcat 
+
+if [ -e "$HOME/../usr/bin/libwebp" ]; then
+	echo -e " ${GREEN}libwebp detectado prosseguindo..."
+	sleep 1
+	else
+		echo -e " ${RED} libwebp não detectado, instalando..."
+echo -e " ${NOCOLOR}"
+echo -e " ${PURPLE} Usando o comando: pkg install libwebp"
+echo -e " ${GREEN}"
+sleep 1
+pkg install libwebp -y
+fi
+sleep 3
+clear
+figlet -c -f slant -t 'Tiringa-BOT' | lolcat 
+
+if [ -e "$HOME/../usr/bin/ffmpeg" ]; then
+	echo -e " ${GREEN}ffmpeg detectado prosseguindo..."
+	sleep 1
+	else
+		echo -e " ${RED} ffmpeg não detectado, instalando..."
+echo -e " ${NOCOLOR}"
+echo -e " ${PURPLE} Usando o comando: pkg install ffmpeg"
+echo -e " ${GREEN}"
+sleep 1
+pkg install ffmpeg -y
+fi
+sleep 3
+clear
+figlet -c -f slant -t 'Tiringa-BOT' | lolcat 
+
+if [ -e "$HOME/../usr/bin/wget" ]; then
+	echo -e " ${GREEN}wget detectado prosseguindo..."
+	sleep 1
+	else
+		echo -e " ${RED} wget não detectado, instalando..."
+echo -e " ${NOCOLOR}"
+echo -e " ${PURPLE} Usando o comando: pkg install wget"
+echo -e " ${GREEN}"
+sleep 1
+pkg install wget -y
+fi
+sleep 3
+clear
+figlet -c -f slant -t 'Tiringa-BOT' | lolcat 
+
+echo -e " ${NOCOLOR}"
+echo -e " ${PURPLE} Usando o comando: pkg install tesseract "
+echo -e " ${GREEN}"
+sleep 1
+pkg install tesseract 
+sleep 3
+clear
+
+
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+figlet -c -f slant -t 'Tiringa-BOT' | lolcat 
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+
+echo -e " ${YELLOW} Todas as dependências foram instaladas."
 echo -e " ${YELLOW} Atualização: mudanças mínimas"
-echo -e " ${YELLOW} Agora é contigo ${GREEN}a_Sol616 ${YELLOW}fazer o NPM no bruto."
-fi
-if [ "$inp" = "n" ]; then
-    echo ${RED}"Okay. inté"
-fi
-sleep 2
+echo -e " ${YELLOW} Use o comando: ${GREEN}npm start ${YELLOW}para iniciar o bot."
